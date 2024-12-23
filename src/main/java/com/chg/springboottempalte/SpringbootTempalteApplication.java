282c9a0c78005cc602bf49e8b0c309292a2990af
chg <c@chgdeMacBook-Air.local>
2024-12-23 23:18:36 +0800

    Initial

---
+++ src/main/java/com/chg/springboottempalte/SpringbootTempalteApplication.java
@@ -0,0 +1,19 @@
+package com.chg.springboottempalte;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+
+@SpringBootApplication
+@MapperScan("com.chg.springboottempalte.mapper")
+// 当 exposeProxy = true 时，Spring AOP 会将当前的 AOP 代理暴露到一个线程上下文中。这意味着在当前线程中，可以通过 AopContext.currentProxy() 方法获取当前代理对象。
+@EnableAspectJAutoProxy(exposeProxy = true)
+public class SpringbootTempalteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootTempalteApplication.class, args);
+    }
+
+}
